81b96813af8e90df17cbd55647aa82ff59fc77d5
Bob Jarvis <bob@zeal.co>
2020-06-16 16:51:29 +0930

    bank shifting, favicons

--- public/script.js
+++ public/script.js
@@ -11,21 +11,29 @@ const box = new Box(0,0,0);
 var players = [];
 // var mp3Locations = [];
 var currentStep = 0;
-var numSteps = 176
+var numSteps = 6
+var bank = 0;
+var numBanks = 8;
+
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
-function setup(){
-  
+function loadBank(){
   for(var i = 1; i <= numSteps; i++){
-    let player = new Tone.Player("../assets/1/00" + i + ".mp3");
+    let fileString = "../assets/" + (bank + 1) +"/00" + i + ".mp3";
+    let player = new Tone.Player(fileString);
     player.toMaster();
     players.push(player);
     // mp3Locations.push("../assets/1/00" + i + ".mp3")
   }
+}
+
+
+function setup(){
+  loadBank();
+
   // console.log(mp3Locations);
   // players = new Tone.Players(mp3Locations);
   // players.toMaster();
@@ -41,17 +49,40 @@ function setup(){
 
 function draw(){
   clear();
+  //change visualisation in response to bank
   box.drawBox();
 }
 
 function step(){
   currentStep++;
-  if(currentStep > numSteps){
+  if(currentStep >= numSteps){
     currentStep = 1;
   }
   players[currentStep].start();
 
 }
 
+function bankNext(){
+  console.log('bank next');
+
+
+  //for each in players
+  //dispose
+  players.forEach(player => player.dispose());
+
+  //clear players
+  players = [];
+
+  //increment bank
+  bank++;
+  bank %= numBanks;
+
+  console.log("bank " + bank);
+
+  //load sounds
+  loadBank();
+}
+
 // document.getElementById('playbutton').onclick = step;
 document.getElementById('playbutton').onmousedown = step;
+document.getElementById('nextButton').onmousedown = bankNext;
